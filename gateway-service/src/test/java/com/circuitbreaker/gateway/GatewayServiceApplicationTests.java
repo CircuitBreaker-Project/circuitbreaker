e5ad46d497435f0f7ffa35b5d82fae24cabd543b
Vengala Manasa <vengalamanasa57@gmail.com>
2026-09-06 13:06:40 +0530

    add gateway application context test

---
+++ gateway-service/src/test/java/com/circuitbreaker/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.circuitbreaker.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    public GatewayServiceApplicationTests() {
+        super();
+        //TODO Auto-generated constructor stub
+    }
+
+    @Test
+    void contextLoads() {
+    }
+}
